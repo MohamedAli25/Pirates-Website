81b071c77c503f4fb40c75aa91921a3ca1943720
Ahmed <ahmedreda4000@yahoo.com>
2020-04-04 11:00:15 +0200

    fix_navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -16,23 +16,42 @@
     <button class="btn bars d-md-none" onclick="opensidebar()"><i class="fa fa-bars"></i>
     </button>
 </nav>
-<nav class="sidenav" id="sidenavbar">
-    <ul class="nav flex-column sidenavbar">
-        <li><a href="#"class="nav-link">Home</a></li>
-        <li><a href="#"class="nav-link">Events</a></li>
-        <li><a href="#"class="nav-link">Committess</a></li>
-        <li><a href="#"class="nav-link">About</a></li>
-        <li><a href="#"class="nav-link">Sign In</a></li>
-        <li><a href="#"class="nav-link">Sign Up</a></li><br>
+<nav class="sidenav" id="sidenavbar" style="width: 0%;">
+    <ul class="navbar-nav flex-column sidenavbar">
+        <li class="sidebartext"><a href="#">Home</a></li>
+        <li class="sidebartext"><a href="#">Events</a></li>
+        <li class="sidebartext"><a href="#">Committess</a></li>
+        <li class="sidebartext"><a href="#">About</a></li>
+        <li class="sidebartext"><a href="#">Sign In</a></li>
+        <li class="sidebartext"><a href="#">Sign Up</a></li>
     </ul>
 </nav>
 <link rel="stylesheet" href="/css/Shared/navbar.css">
 <script>
     function opensidebar(){
+        if(document.getElementById('sidenavbar').style.width === '0%'){
         var sidebar = document.getElementById('sidenavbar');
         var body = document.getElementById('content');
         sidebar.style.width = "60%";
         body.style.backgroundColor = "rgba(0,0,0,0.4)";
+       
+        var elements = document.getElementsByClassName('sidebartext');
+        for(var i=0; i<elements.length; i++) { 
+            elements[i].classList.add('tracking');
+            elements[i].classList.remove('trackingo');
+        }
+        
+        }else{
+        var sidebar = document.getElementById('sidenavbar');
+        var body = document.getElementById('content');
+        sidebar.style.width = "0%";
+        body.style.backgroundColor = "rgba(0,0,0,0)";
+        var elements = document.getElementsByClassName('sidebartext');
+        for(var i=0; i<elements.length; i++) { 
+            elements[i].classList.remove('tracking');
+            elements[i].classList.add('trackingo');
+        }         
+        }
     }
 </script>
 </div>
