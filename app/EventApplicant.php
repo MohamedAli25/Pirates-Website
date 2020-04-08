2bf58e121465c675903272a8b5c7410a70b72cc4
Ahmed <ahmedreda4000@yahoo.com>
2020-04-08 07:53:45 +0200

    added_some_relations

--- app/EventApplicant.php
+++ app/EventApplicant.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class EventApplicant extends Model
 {
-    //
+    public function TimeSlot(){
+        return $this->hasOne('app/TimeSlot');
+    }
 }
--- app/TimeSlot.php
+++ app/TimeSlot.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class TimeSlot extends Model
 {
-    //
+    public function Event(){
+        return $this->belongsTo('app/Event');
+    }
+
+    public function EventApplicant(){
+        return $this->belongsToMany('app/EventApplicant');
+    }
 }
