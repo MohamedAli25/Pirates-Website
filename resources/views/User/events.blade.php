0f68f4e8a9ecfd4752a25942d632b8cf9ed38ff1
Doaahesham <doaahesham1399@gmail.com>
2020-04-12 17:07:58 +0200

    events page

---
+++ resources/views/User/events.blade.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html>
+<head>
+   
+    <meta charset="utf-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+ <link rel="stylesheet" type="text/css" href="events page.css"> 
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+ <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+ <link href="https://fonts.googleapis.com/css?family=Rubik&display=swap" rel="stylesheet">
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+ <title>Pirates Events</title>
+
+ 
+</head>
+
+<body>
+   
+      
+    <div class="topnav">
+      <a href="#" id="back1">HOME</a>
+      <a href="#"><span class="glyphicon glyphicon-user"></span>ABOUT</a>
+      <a href="#"><span class="glyphicon glyphicon-user"></span>PORTIFLIO</a>
+      <a href="#"><span class="glyphicon glyphicon-envelope"></span>CONTACT</a>
+      <p id="p1"> Pirates Egypt</p>
+    </div>
+<p class="p2">1-Technates</p>
+    <div class="row">
+        <div class="col-sm-6" class="div1"><br><img src="parallax1.jpg" class="img1"></div>
+         <p class="col-sm-6" id="p3"> 1- Technates (Technical event) :
+          <br>In this event, we provide engineering students with technical
+          workshops in all departments (Mechanical- Electrical – Civil –
+          Architecture).<br>
+          Technates aims to support you with the experience and the
+          knowledge you really need in order to start working after
+          graduation & improve your technical skills.
+          <br>It includes:<br>
+          • Offline launch during the first semester.<br>
+          • Competition between the participants after the workshops.<br>
+          
+          Example:<br>
+          Arduino for electrical department<br>
+          Autocad for architecture department<br>
+          Solidworks for mechanical department</p>
+
+       </div>
+      <br>
+
+       <p class="p2">2-Breach your limit “Seminar”</p>
+       <div class="row">
+         <p class="col-sm-6" id="p4"> In this seminar, we focus on people who started successful
+          startups and invite them to share with the attendees their
+          experience.<br>
+          Also we may have people who have an achievements or
+          influencers that students are interested to attend a seminar for
+          them<br>
+          It includes:<br>
+          •Offline launch.<br>
+          • Seminar ceremony<br>
+          Last year we have 3 speaker<br>
+          1- Kareem Esmail<br>
+          2- Dr. Shady Elzyat<br>
+          3- Ahmed Gouda</p>
+     <div class="col-sm-4" class="div2"><img src="parallax2.jpg" class="img2"></div>
+    
+       </div>
+<br>
+<p class="p2">3-Business Engineer</p>
+       <div class="row">
+        <div class="col-sm-6" class="div1"><img src="parallax3.jpg" class="img1"></div>
+         <p class="col-sm-6" id="p3">In this event, we provide students with non-technical workshops
+          in all the aspects they need to build their own startup.there will
+          be a competition merged between the workshops to apply on
+          what they have learned in this sessions<br>
+          It includes:<br>
+          • Offline launch during the second semester.<br>
+          • Competition between the participants after the workshops.<br>
+          Examples on workshops:<br>
+          1- Project management<br>
+          2- Sales &Business<br>
+          3- Photoshop</p>
+
+       </div>
+<br>
+<p class="p2">4-Career intelligence “Seminar”</p>
+       <div class="row">
+        <p class="col-sm-6" id="p4">In this seminar, we focus on engineers who works in a great
+          companies to share with the attendees their experience and how
+          to develop his skills in his department. Also they talk about the
+          companies and what is the suitable requirements to apply in it.<br>
+          It includes:<br>
+          •Offline launch.<br>
+          • Seminar ceremony</p>
+    <div class="col-sm-4" class="div2"><img src="p3.jpg" class="img2"></div>
+  
+      </div>
+
+<br>
+
+<footer>
+        <div class="social-menu">
+      
+        <ul>
+          <li><a href=""><i class="fa fa-facebook"></i></a></li>
+          <li><a href=""><i class="fa fa-twitter"></i></a></li>
+          <li><a href=""><i class="fa fa-instagram"></i></a></li>
+          <li><a href=""><i class="fa fa-youtube"></i></a></li>
+          <li><a href=""><i class="fa fa-linkedin"></i></a></li>
+        </ul>
+      </div>
+      
+      
+      <br>
+      <br>
+            <!-- Copyright -->
+          <div class="footer-copyright text-center py-3">© 2020 Copyright:
+            <a href="#"> Pirates Egypt</a>
+          </div>
+       
+        
+        </footer>
+        
+
+
+
+</body>
+
+
+
+</html>
