b230f9531d18100355802bb9935cb9f180a3075e
theMeanScore <m.ad.ali2165@gmail.com>
2020-08-06 11:09:45 +0200

    Finish authorization user functions

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -7,6 +7,29 @@ use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
+    // Positions IDs
+    public const memberPositionId = 0;
+    public const viceHeadPositionId = 0;
+    public const headPositionId = 0;
+    public const executiveDirectorPositionId = 0;
+    public const organizationManagementDirectorPositionId = 0;
+    public const vicePresidentPositionId = 0;
+    public const presidentPositionId = 0;
+    public const projectManagerPositionId = 0;
+
+    // Committees IDs
+    public const frCommitteeId = 0;
+    public const itCommitteeId = 0;
+    public const marketingCommitteeId = 0;
+    public const qmCommitteeId = 0;
+    public const hrCommitteeId = 0;
+    public const advCommitteeId = 0;
+    public const lrCommitteeId = 0;
+    public const technicalAcCommitteeId = 0;
+    public const nonTechnicalAcCommitteeId = 0;
+    public const pmCommitteeId = 0;
+    public const tmCommitteeId = 0;
+
     /**
      * The policy mappings for the application.
      *
--- app/User.php
+++ app/User.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\Providers\AuthServiceProvider;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -45,5 +46,223 @@ class User extends Authenticatable
     public function events()
     {
         return $this->belongsToMany(Event::class, 'event_applicants');
+    }
+
+    public function crewMember()
+    {
+        return $this->hasOne(CrewMember::class);
+    }
+
+    // Crew Member Check
+    public function isCrewMember()
+    {
+        return CrewMember::where('user_id', $this->id)->isNotEmpty();
+    }
+
+    // Position Check
+    public function isMember()
+    {
+        if ($this->isCrewMember()) {
+            return $this->crewMember()->position_id === AuthServiceProvider::memberPositionId;
+        } else {
+            return false;
+        }
+    }
+
+    public function isViceHead()
+    {
+        if ($this->isCrewMember()) {
+            return $this->crewMember()->position_id === AuthServiceProvider::viceHeadPositionId;
+        } else {
+            return false;
+        }
+    }
+
+    public function isHead()
+    {
+        if ($this->isCrewMember()) {
+            return $this->crewMember()->position_id === AuthServiceProvider::headPositionId;
+        } else {
+            return false;
+        }
+    }
+
+    public function isPM()
+    {
+        if ($this->isCrewMember()) {
+            return $this->crewMember()->position_id === AuthServiceProvider::pmCommitteeId;
+        } else {
+            return false;
+        }
+    }
+
+    public function isExecutiveDirector()
+    {
+        if ($this->isCrewMember()) {
+            return $this->crewMember()->position_id === AuthServiceProvider::executiveDirectorPositionId;
+        } else {
+            return false;
+        }
+    }
+
+    public function isOMDirector()
+    {
+        if ($this->isCrewMember()) {
+            return $this->crewMember()->position_id === AuthServiceProvider::organizationManagementDirectorPositionId;
+        } else {
+            return false;
+        }
+    }
+
+    public function isVicePresident()
+    {
+        if ($this->isCrewMember()) {
+            return $this->crewMember()->position_id === AuthServiceProvider::vicePresidentPositionId;
+        } else {
+            return false;
+        }
+    }
+
+    public function isPresident()
+    {
+        if ($this->isCrewMember()) {
+            return $this->crewMember()->position_id === AuthServiceProvider::presidentPositionId;
+        } else {
+            return false;
+        }
+    }
+
+    // Committee Check
+    public function isInFR()
+    {
+        if ($this->isCrewMember()) {
+            return $this->crewMember()->committee_id === AuthServiceProvider::frCommitteeId;
+        } else {
+            return false;
+        }
+    }
+
+    public function isInIT()
+    {
+        if ($this->isCrewMember()) {
+            return $this->crewMember()->committee_id === AuthServiceProvider::itCommitteeId;
+        } else {
+            return false;
+        }
+    }
+
+    public function isInMarketing()
+    {
+        if ($this->isCrewMember()) {
+            return $this->crewMember()->committee_id === AuthServiceProvider::marketingCommitteeId;
+        } else {
+            return false;
+        }
+    }
+
+    public function isInQM()
+    {
+        if ($this->isCrewMember()) {
+            return $this->crewMember()->committee_id === AuthServiceProvider::qmCommitteeId;
+        } else {
+            return false;
+        }
+    }
+
+    public function isInHR()
+    {
+        if ($this->isCrewMember()) {
+            return $this->crewMember()->committee_id === AuthServiceProvider::hrCommitteeId;
+        } else {
+            return false;
+        }
+    }
+
+    public function isInAdv()
+    {
+        if ($this->isCrewMember()) {
+            return $this->crewMember()->committee_id === AuthServiceProvider::advCommitteeId;
+        } else {
+            return false;
+        }
+    }
+
+    public function isInLR()
+    {
+        if ($this->isCrewMember()) {
+            return $this->crewMember()->committee_id === AuthServiceProvider::lrCommitteeId;
+        } else {
+            return false;
+        }
+    }
+
+    public function isInTechnicalAc()
+    {
+        if ($this->isCrewMember()) {
+            return $this->crewMember()->committee_id === AuthServiceProvider::technicalAcCommitteeId;
+        } else {
+            return false;
+        }
+    }
+
+    public function isInNonTechnicalAc()
+    {
+        if ($this->isCrewMember()) {
+            return $this->crewMember()->committee_id === AuthServiceProvider::nonTechnicalAcCommitteeId;
+        } else {
+            return false;
+        }
+    }
+
+    public function isInPm()
+    {
+        if ($this->isCrewMember()) {
+            return $this->crewMember()->committee_id === AuthServiceProvider::pmCommitteeId;
+        } else {
+            return false;
+        }
+    }
+
+    public function isInTM()
+    {
+        if ($this->isCrewMember()) {
+            return $this->crewMember()->committee_id === AuthServiceProvider::tmCommitteeId;
+        } else {
+            return false;
+        }
+    }
+
+    // Specific Committee Board Check
+    public function isCommitteeViceHead(Committee $committee)
+    {
+        if ($this->isCrewMember()) {
+            return $this->isViceHead() && $this->crewMember()->committee()->is($committee);
+        } else {
+            return false;
+        }
+    }
+
+    public function isCommitteeHead(Committee $committee)
+    {
+        if ($this->isCrewMember()) {
+            return $this->isHead() && $this->crewMember()->committee()->is($committee);
+        } else {
+            return false;
+        }
+    }
+
+    public function isCommitteeBoard(Committee $committee)
+    {
+        return $this->isCommitteeViceHead($committee) || $this->isCommitteeHead($committee);
+    }
+
+    // General Committee and Position Check
+    public function isIn(Committee $committee, Position $position)
+    {
+        if ($this->isCrewMember()) {
+            return $this->crewMember()->committee()->is($committee) && $this->crewMember()->position()->is($position);
+        } else {
+            return false;
+        }
     }
 }
